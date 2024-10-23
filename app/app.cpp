d52de9e1d9ded46d59fde7fcf3961801e89fa662
breski-igor <igor.breski@vsite.hr>
2024-10-23 07:49:13 +0200

    v01 gotova

--- app/app.cpp
+++ app/app.cpp
@@ -1,11 +1,48 @@
-#include "app.h"
+﻿#include "app.h"
 #include <format>
+#include <ostream>
+#include <iomanip>
 
 namespace vsite::oop::v1
 {
 /*
 * function implementations
 */
+    std::string to_hex(int a) {
+        return std::format("{:X}", a);
+    }
+    std::string to_exp(double b) {
+        return std::format("{:.2e}", b);
+    }
+    void mult_table(int c, std::ostream& os) {
+        if (c <= 0 || c > 20) {
+            os << "";
+            return;
+        }
+
+        if (c == 1) {
+            os << "0   1\n1   1\n";
+            return;
+        }
+       
+        os << 0;
+        for (int j = 1; j <= c; j++) {
+            os << std::setw(4) << j;
+        }
+        os << '\n';
+
+        for (int i = 1; i <= c; i++) {
+            os << i;
+            for (int j = 1; j <= c; j++) {
+                os << std::setw(4) << i * j;
+            }
+            os << '\n';
+        }
+    }
+        
+
+    
+    
 
 } // namespace
 
--- app/main.cpp
+++ app/main.cpp
@@ -4,6 +4,16 @@
 // write its hex representation to std::cout (using vsite::oop::v1::to_hex)
 // write multiplication table up to this number to std::cout (using vsite::oop::v1::mult_table)
 
+using namespace vsite::oop::v1;
+
 int main()
 {
+	int n;
+	std::cout << "unesi broj: ";
+	std::cin >> n;
+	std::cout << "hex: " << to_hex(n) << std::endl;
+	std::cout << "tablica mnozenja do: " << n << std::endl;
+
+	mult_table(n, std::cout);
+
 }
