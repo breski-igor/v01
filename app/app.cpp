40c8bff3393609b432fbf0084234c981f23dfac4
breski-igor <igor.breski@vsite.hr>
2024-10-23 11:22:59 +0200

    v01 promjena 1

--- app/app.cpp
+++ app/app.cpp
@@ -15,29 +15,22 @@ namespace vsite::oop::v1
         return std::format("{:.2e}", b);
     }
     void mult_table(int c, std::ostream& os) {
-        if (c <= 0 || c > 20) {
-            os << "";
-            return;
-        }
-
-        if (c == 1) {
-            os << "0   1\n1   1\n";
-            return;
-        }
-       
-        os << 0;
-        for (int j = 1; j <= c; j++) {
-            os << std::setw(4) << j;
-        }
-        os << '\n';
-
-        for (int i = 1; i <= c; i++) {
-            os << i;
+        if (c > 0 && c <= 20) {
+            os << 0;
             for (int j = 1; j <= c; j++) {
-                os << std::setw(4) << i * j;
+                os << std::setw(4) << j;
             }
             os << '\n';
+
+            for (int i = 1; i <= c; i++) {
+                os << i;
+                for (int j = 1; j <= c; j++) {
+                    os << std::setw(4) << i * j;
+                }
+                os << '\n';
+            }
         }
+        return;
     }
         
 
